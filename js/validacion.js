a80d92a26e16d04aa07685e6919510531d89930d
DaniGG832 <daniel.gonzalez.garcia@iesdonana.org>
2021-12-05 10:42:17 +0100

    validacion7

--- js/validacion.js
+++ js/validacion.js
@@ -40,19 +40,23 @@ document.getElementById("area").addEventListener("blur", validartextarea);
 
 /* validar formulario y mandar o parar en caso de faltar algun requisito */
 function validar(e) {
+
   if (
     validarnombre() &&
     validarapellido() &&
     validartelefono() &&
     validaremail() &&
     validartextarea()
-    
-  ){ 
+ 
+  ){   
+
     if(!confirm("Deseas mandar los datos del formulario")){
       e.preventDefault();
       console.log("el formulario no se ha mandado");
       return true;
+
     }else{
+
       return false;
     }
     
@@ -67,10 +71,14 @@ function validar(e) {
 
 /* Habilitar el textarea cuando todos los campos anteriores no estén validados correctamente */
 function habilitar(e) {
+
   if (validarnombre() && validarapellido() && validaremail() && validartelefono()) {
+
     console.log("texarea");
     document.getElementById("area").disabled = false;
+
   } else {
+
     document.getElementById("area").disabled = true;
   }
 }
@@ -78,14 +86,19 @@ function habilitar(e) {
 /* validar nombre */
 
 function validarnombre(e) {
+
   let elemento = document.getElementById("nombre");
   elemento.setCustomValidity("");
+
   if (!elemento.checkValidity()) {
+
     textoError(elemento);
     error(elemento, "errornombre"); 
     /* elemento.reportValidity(); */
     return  false;
+
   } else {
+
     borrarerror(elemento, "errornombre");
     return true;
   }
@@ -95,14 +108,18 @@ function validarnombre(e) {
 /* validar apellido */
 
 function validarapellido(e) {
+
   let elemento = document.getElementById("apellidos");
   elemento.setCustomValidity("");
+
   if (!elemento.checkValidity()) {
+
     textoError(elemento);
     error(elemento, "errorapellido");
-    
     return false;
+
   } else {
+
     borrarerror(elemento, "errorapellido");
     return true;
   }
@@ -111,38 +128,47 @@ function validarapellido(e) {
 /* validar telefono  */
 
 function validartelefono(e) {
+
   let elemento = document.getElementById("telefono");
   elemento.setCustomValidity("");
   console.log(!elemento.checkValidity());
+
   if (!elemento.checkValidity()) {
+
     textoError(elemento);
     error(elemento, "errortlf");
-    
     return false;
-  } else {
-    borrarerror(elemento, "errortlf");
 
+  } else {
+
+    borrarerror(elemento, "errortlf");
     return true;
   }
 }
 
 /* pasar a minusculas email en el caso que este activado bloq mayus */
 function emailminusculas() {
+
   let str = document.getElementById("email").value;
   document.getElementById("email").value = str.toLowerCase();
+
 }
 
 /* validar email */
 function validaremail(e) {
+
   let elemento = document.getElementById("email");
   console.log(!elemento.checkValidity());
   elemento.setCustomValidity("");
+
   if (!elemento.checkValidity()) {
+
     textoError(elemento);
     error(elemento, "erroremail");
     return false;
 
   } else {
+
     borrarerror(elemento, "erroremail");
     return true;
   }
@@ -150,34 +176,43 @@ function validaremail(e) {
 
 /* avisar al usuario con un alert cuando pulse shift + una tecla del teclado */
 function emailMay(e) {
+
   emailminusculas();
 
   if (e.shiftKey) {
+
     document.getElementById("erroremail").innerHTML =
       "No se permiten mayusculas";
     alert("No esta permitido mayusculas en le correo");
+
   } else {
+
     document.getElementById("erroremail").innerHTML = "";
   }
 }
 
 /* validar texarea */
 function validartextarea(e) {
+  
   let elemento = document.getElementById("area");
   
   if (!elemento.checkValidity()) {
    
     document.getElementById("mensajeTError").style.color = "red";
     if (elemento.validity.valueMissing) {
+
       document.getElementById("mensajeTError").innerHTML =
         "Debe de tener minimo 10 caracteres.";
         
     } else {
+
       error(elemento, "mensajeTError");
     }
 
     return false;
+
   } else {
+
     borrarerror(elemento, "mensajeTError");
 
     return true;
@@ -194,6 +229,7 @@ function error(elemento, id) {
 
 /* borrar mensaje error  */
 function borrarerror(elemento, mensaje) {
+
   document.getElementById(mensaje).innerHTML = "";
 }
 
@@ -202,8 +238,10 @@ function borrarerror(elemento, mensaje) {
 function textoError(elemento) {
   
   if (elemento.validity.valueMissing){ //"true" si elemento es vacio y es "required"
+
     elemento.setCustomValidity( "El campo "+ elemento.id +" es obligatorio.");
     }
+    
     if (elemento.validity.patternMismatch){ //"true" si elemento no coincide con patrón
     elemento.setCustomValidity( "Debe introducir " +elemento.id+ " con el formato correcto." );
     }
